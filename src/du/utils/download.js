35fd0a68410b770d394f0df274e0336575892b0a
zhangshuyao <zh1045456074@163.com>
2019-09-03 18:46:47 +0800

    download

--- src/du/utils/download.js
+++ src/du/utils/download.js
@@ -1,6 +1,26 @@
 
-export function saveFile () {
+import xlsx from 'node-xlsx'
+import FileSaver from 'file-saver'
+import { Readable } from 'stream'
 
+export function saveFile () {
+  FileSaver.saveAs()
 }
 
-export function generateExcel () {}
+export function generateExcel (data, options = {}) {
+  const name = '报表'
+  const rs = new Readable()
+  const buffer = xlsx.build([{ name, data }], options)
+  const filename = `${encodeURIComponent(name)}.xlsx`
+
+  console.log(rs, buffer, filename)
+
+  // res.setHeader('Content-Disposition', `attachment; filename=${filename}`)
+  // // res.setHeader('Content-type', 'application/x-msdownload')
+  // res.setHeader('Content-Type', 'application/vnd.ms-excel')
+  // res.setHeader('X-Suggested-Filename', filename)
+
+  // rs.push(buffer)
+  // rs.push(null)
+  // rs.pipe(res)
+}
